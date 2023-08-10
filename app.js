e052e2df0048f466545ce494ef2c0c6b328debd5
rihamissa123 <riham.issa123@gmail.com>
2023-08-10 11:52:50 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors=require("cors")
+const xlsx=require("xlsx")
+const app = express()
+app.use(cors())
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+app.get('/services',(req, res)=> {
+  
+  let wb=xlsx.readFile("data.xlsx");
+  let ws=wb.Sheets["services"];
+  let data=xlsx.utils.sheet_to_json(ws);
+  res.send(data);
+
+})
+
+app.listen(3000)
